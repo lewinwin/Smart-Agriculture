750bc6983efcb7bf02f3ace2c9d08c7eaf5021b8
Lê Win <winle55555@gmail.com>
2024-07-29 11:00:58 +0700

    Update Thingspeak_IOT_GARDEN.py

--- Thingspeak_IOT_GARDEN.py
+++ Thingspeak_IOT_GARDEN.py
@@ -5,11 +5,13 @@ from machine import Pin,ADC
 import network, time
 import dht
 
-dht_sensor = dht.DHT11(machine.Pin(33)) #kết nối cảm biến dht với chân 23 (A2 trên Shield)
-soil = ADC(Pin(32)) # kết nối cảm biến với chân 34 (A1 trên shield)
+dht_sensor = dht.DHT11(machine.Pin(34)) #kết nối cảm biến dht với chân 34 (A1 trên Shield)
+soil = ADC(Pin(32)) # kết nối cảm biến với chân 32 (A3 trên shield)
 soil.atten(ADC.ATTN_11DB)
-light = ADC (Pin(34))
+light = ADC (Pin(33)) #kết nối cảm biến với chân 33 (A2 trên shield)
 light.atten(ADC.ATTN_11DB)
+led = Pin(2, Pin.OUT)
+pump = Pin(3, Pin.OUT)
 
 #Thông tin tài khoản Thingspeak 
 HTTP_HEADERS = {'Content-Type': 'application/json'} 
@@ -41,7 +43,20 @@ while True:
     print(sensor_reading) #in 2 giá trị gửi
     request = urequests.post( 'http://api.thingspeak.com/update?api_key=' + THINGSPEAK_WRITE_API_KEY,json = sensor_reading, headers = HTTP_HEADERS )  
     request.close()
-    
+    controlLED()
+    controlWaterPump()
+    request.close()
 		#đọc giá trị từ field5 (nút nhấn) và bật-tắt đèn tương ứng
     
     time.sleep(2)
+def controlLED ():
+    if (light.read()<200):
+        led.value(1)
+    else:
+        led.value(0)
+def controlWaterPump ():
+    if (soil.read() <512):
+        pump.value(1)
+        while (soil.read()<614):
+            time.sleep(1)
+        pump.value(0)
