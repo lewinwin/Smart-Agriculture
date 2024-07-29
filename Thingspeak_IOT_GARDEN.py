84651019765725a23ed937721baa5fd30fc29a81
Lê Win <winle55555@gmail.com>
2024-07-29 18:18:15 +0700

    Update Thingspeak_IOT_GARDEN.py

--- Thingspeak_IOT_GARDEN.py
+++ Thingspeak_IOT_GARDEN.py
@@ -6,10 +6,10 @@ import network, time
 import dht
 
 dht_sensor = dht.DHT11(machine.Pin(33)) #kết nối cảm biến dht với chân 33 (A2 trên Shield)
-soil = ADC(Pin(32)) # kết nối cảm biến với chân 32 (A3 trên shield)
-soil.atten(ADC.ATTN_11DB)
-light= ADC (Pin(34)) #kết nối cảm biến với chân 34 (A1 trên shield)
-light.atten(ADC.ATTN_11DB)
+soil = machine.ADC(machine.Pin(34)) # kết nối cảm biến với chân 34 (A1 trên shield)
+soil.atten(machine.ADC.ATTN_11DB)
+light= machine.ADC(machine.Pin(32)) #kết nối cảm biến với chân 32 (A3 trên shield)
+light.atten(machine.ADC.ATTN_11DB)
 led_pin=13
 led = Pin(led_pin, Pin.OUT)
 
@@ -35,7 +35,7 @@ print('network config:', sta_if.ifconfig())
 field1_request = 'https://api.thingspeak.com/channels/2611173/fields/1/last.json'
 
 def controlLED ():
-    if light_value < 200:
+    if light_value < 2000:
         led.value(0)
     else:
         led.value(1)
@@ -45,15 +45,14 @@ while True:
     humidity = dht_sensor.humidity() #đọc giá trị độ ẩm
     soil_value=soil.read()
     light_value=light.read()
-    sensor_reading = {'field1':temperature, 'field2':humidity, 'field3':soil_value, 'field4':light_value} #nối các giá trị thành 1 chuỗi để gửi 
+    sensor_reading = {'field1':temperature, 'field2':humidity, 'field3':light_value, 'field5':soil_value} #nối các giá trị thành 1 chuỗi để gửi 
     print(sensor_reading) #in 2 giá trị gửi
-    print (light_value)
     request = urequests.post( 'http://api.thingspeak.com/update?api_key=' + THINGSPEAK_WRITE_API_KEY,json = sensor_reading, headers = HTTP_HEADERS )  
-    request.close()    
-    controlLED ()
+    request.close()
+    controlLED()
     #đọc giá trị từ field5 (nút nhấn) và bật-tắt đèn tương ứng
     
-    time.sleep(0.1)
+    time.sleep(5)
 '''from machine import Pin
 import time
 led_pin=13
