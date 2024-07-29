d100f99738cc92431528eed064e34c6470773851
Lê Win <winle55555@gmail.com>
2024-07-29 21:28:33 +0700

    Update Thingspeak_IOT_GARDEN.py

--- Thingspeak_IOT_GARDEN.py
+++ Thingspeak_IOT_GARDEN.py
@@ -4,13 +4,27 @@ import ujson as json
 from machine import Pin,ADC
 import network, time
 import dht
+from machine import SoftI2C
+from lcd_api import LcdApi
+from i2c_lcd import I2cLcd
+from time import sleep
+
+
+I2C_ADDR = 0x27
+totalRows = 2
+totalColumns = 16
+
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=10000)     #initializing the I2C method for ESP32
+#i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)       #initializing the I2C method for ESP8266
+
+lcd = I2cLcd(i2c, I2C_ADDR, totalRows, totalColumns)
 
 dht_sensor = dht.DHT11(machine.Pin(33)) #kết nối cảm biến dht với chân 33 (A2 trên Shield)
 soil = machine.ADC(machine.Pin(34)) # kết nối cảm biến với chân 34 (A1 trên shield)
 soil.atten(machine.ADC.ATTN_11DB)
 light= machine.ADC(machine.Pin(32)) #kết nối cảm biến với chân 32 (A3 trên shield)
 light.atten(machine.ADC.ATTN_11DB)
-led_pin=13
+led_pin=13 # chân D9
 led = Pin(led_pin, Pin.OUT)
 
 
@@ -50,6 +64,20 @@ while True:
     request = urequests.post( 'http://api.thingspeak.com/update?api_key=' + THINGSPEAK_WRITE_API_KEY,json = sensor_reading, headers = HTTP_HEADERS )  
     request.close()
     controlLED()
+    lcd.move_to(0,0)
+    lcd.putstr("Temp:")
+    lcd.putstr(str(temperature))
+    lcd.putstr("  ")
+    lcd.putstr("Hum:")
+    lcd.putstr(str(humidity))
+    lcd.move_to(0,1)
+    lcd.putstr("Li:")
+    lcd.putstr(str(light_value))
+    lcd.putstr(" ")
+    lcd.putstr("Soil:")
+    lcd.putstr(str(soil_value))
+    lcd.putstr(" ")
+    
     #đọc giá trị từ field5 (nút nhấn) và bật-tắt đèn tương ứng
     
     time.sleep(5)
